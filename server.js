20c80f1f7765bc8c60e8a40b587e5d8318236b45
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2023-05-22 08:11:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 let drawings = [];
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   // Enviar desenhos anteriores para o novo cliente
   drawings.forEach((drawing) => {
-    socket.emit("draw", drawing);
+    socket.emit('draw', drawing);
   });
 
-  socket.on("draw", (data) => {
+  socket.on('draw', (data) => {
     drawings.push(data);
-    socket.broadcast.emit("draw", data); // é chamado para enviar o desenho para todos os outros clientes conectados, exceto o cliente atual.
+    socket.broadcast.emit('draw', data);
+  });
+
+  socket.on('clear', () => {
+    drawings = []; // Limpa o array de desenhos
+    io.emit('clear'); // Emite o evento 'clear' para todos os clientes, incluindo o cliente atual
   });
 });
 
 http.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 });
